e249254e24c5a6e25cc03252cd1efa45dfae0607
shivadharshini3103 <shivadharshini3103@gmail.com>
2026-09-01 14:37:09 +0530

    Add files via upload

---
+++ Day 1 Task/script.js
@@ -0,0 +1,23 @@
+let name = "Shiva";
+let age = 22;
+let mark = 90;
+
+console.log(name);
+console.log(age);
+console.log(mark);
+       
+
+let userName = prompt("What is your name?");
+alert(userName);
+
+      
+let userAge = prompt("What is your age?");
+console.log(userAge);
+
+      
+let answer = confirm("Do you know JavaScript?");
+console.log(answer);
+
+
+let fullName = prompt("What is your FullName?");
+document.writeln(fullName);
